c1c94735c8392477a4680ce8f823c8b586540319
Victor <89050826+VictorRTXD@users.noreply.github.com>
2022-12-01 16:53:38 +0000

    ya jala

--- calculadora/Main.java
+++ calculadora/Main.java
@@ -5,34 +5,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int i;
+        short i;
 
         System.out.println("Selecciona la opcion deseada");
         do {
-            System.out.println("1: Carga Eléctrica y Campo Eléctrico");
-            System.out.println("2: Corrientes Eléctricas , Resistencia y Fuerza Electromotriz");
-            System.out.println("3: Circuitos en Corriente Directa");
-            System.out.println("4: Ley de Gauss");
-            System.out.println("5: Potencial Eléctrico");
+            System.out.println("1: Carga Eléctrica ");
+            System.out.println("2: Campo electrico");
+            System.out.println("3: Circuitos");
             System.out.println("0: Salir");
 
-            i = sc.nextInt();
+            i = sc.nextShort();
 
             switch (i) {
                 case 1:
-                    System.out.println("funciona");
+                CargaElectrica ce = new CargaElectrica();
+                ce.leyCoulomb();
                 break;
                 case 2:
-                    System.out.println();
+                CampoElectrico caele = new CampoElectrico();
+                caele.campoElectrico();
                 break;
                 case 3:
-                    System.out.println();
-                break;
-                case 4:
-                    System.out.println();
-                break;
-                case 5:
-                    System.out.println();
+                    Circuitos circuitos = new Circuitos();
+                    circuitos.leyOhm();
                 break;
                 default:
                     System.out.println();
