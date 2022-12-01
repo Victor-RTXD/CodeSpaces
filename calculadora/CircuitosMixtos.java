ba0f958d0a26d154ff80712a39e6c6fc7a079219
Victor <89050826+VictorRTXD@users.noreply.github.com>
2022-12-01 17:18:48 +0000

    haciendo estructura de circuitos

---
+++ calculadora/CircuitosMixtos.java
@@ -0,0 +1,22 @@
+package calculadora;
+
+import java.util.Scanner;
+
+public class CircuitosMixtos {
+    short i = 0;
+    Scanner sc = new Scanner(System.in);
+
+    public void circuitosMixtos() {
+        System.out.println("selecciona lo que deseas saber, \n 1: RT \n 2: VT \n 3: IT \n AT");
+
+        switch (i) {
+            case 1:
+                
+            break;
+        
+            default:
+            
+            break;
+        }
+    }
+}
---
+++ calculadora/CircuitosParalelos.java
@@ -0,0 +1,29 @@
+package calculadora;
+
+import java.util.Scanner;
+
+public class CircuitosParalelos {
+    Scanner sc = new Scanner(System.in);
+    short i = 0;
+
+    public void CircuitosParalelos() {
+        System.out.println("selecciona lo que deseas saber, \n 1: RT \n 2: VT \n 3: IT \n AT");
+
+        switch (i) {
+            case 1:
+                System.out.println("ingresa las resistencias, maximo 5");
+
+                double resistencia[] = new double[6];
+
+                do {
+
+                    System.out.println("ingresar otro?");
+                } while (i != 0);
+            break;
+        
+            default:
+            
+            break;
+        }
+    }
+}
